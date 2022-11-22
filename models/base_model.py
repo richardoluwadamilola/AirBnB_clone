fc342863eb8cb6e94167b640d82a9ea0f37eaf24
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-22 06:41:30 -0800

    base and init file

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Defines the BaseModel class
+
+import models
+from uuid import uuid4
+from datetime import datetime
+
+Class BaseModel:
+    # Represents the BaseModel of thr AirBnB project.
+
+
