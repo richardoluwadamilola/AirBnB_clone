39f7b73d7b3af2201a787299063e14f2bdd7b0e5
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-22 06:46:36 -0800

    base file

--- models/base_model.py
+++ models/base_model.py
@@ -8,4 +8,12 @@ from datetime import datetime
 Class BaseModel:
     # Represents the BaseModel of thr AirBnB project.
 
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a BaseModel.
+        Args:
+        *args(any): Unused.
+        **kwargs(dict): key/value pairs of attributes.
+        """
+
 
