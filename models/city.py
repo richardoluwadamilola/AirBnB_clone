0da04c0235966442053b074c7ac8192e96f8932b
FiifiAsare <107552878+FiifiAsare@users.noreply.github.com>
2022-11-23 00:36:19 +0000

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a User class"""
+
+from models.BaseModel import BaseModel
+
+
+class City(BAseModel):
+  """This class managers cities"""
+  name= " "
+  state_id= " "
