2a0c147c859ab51de979c15673eedebe3db46292
FiifiAsare <107552878+FiifiAsare@users.noreply.github.com>
2022-11-23 01:07:40 +0000

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a amenity class"""
+
+from models.base_model import BaseModel
+
+
+class amenity(BaseModel):
+    """Class for managing amenity objects"""
+
+    name = ""
