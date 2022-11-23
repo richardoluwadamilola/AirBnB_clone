5f6b22d36c478733b85eae7322ef632b778af8e8
FiifiAsare <107552878+FiifiAsare@users.noreply.github.com>
2022-11-23 00:55:32 +0000

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This class creates user class"""
+from models.base_model import BaseModel
+
+class user(BaseModel):
+  """class managers user objects"""
+  
+    email= " "
+  password= " "
+  FirstName= " "
+  LastName= " "
