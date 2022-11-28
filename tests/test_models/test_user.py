150b62adaed085d9da8e5f1b2dcad8362bd28d47
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-11-28 10:09:50 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,20 +9,20 @@ from datetime import datetime
 
 
 class UserCase(unittest.TestCase):
-    #Test instances and methods from user class
+    """Test instances and methods from user class"""
 
     person = User()
 
     def test_class_exists(self):
-        #test if class exist
+        """test if class exist"""
         self.assertEqual(str(type(self.person)), "<class 'models.user.User'>")
 
     def test_user_inheritance(self):
-        #test if the user class is a subclass of the base class/model
+        """test if the user class is a subclass of the base class/model"""
         self.assertIsInstance(self.person, User)
 
     def testHasAttributes(self):
-        #verify if attributes exists
+        """verify if attributes exists"""
         self.assertTrue(hasattr(self.person, 'email'))
         self.assertTrue(hasattr(self.person, 'password'))
         self.assertTrue(hasattr(self.person, 'first_name'))
@@ -32,7 +32,7 @@ class UserCase(unittest.TestCase):
         self.assertTrue(hasattr(self.person, 'updated_at'))
 
     def test_types(self):
-        #tests if the type of the attribute is the correct one
+        """tests if the type of the attribute is the correct one"""
         self.assertIsInstance(self.person.first_name, str)
         self.assertIsInstance(self.person.last_name, str)
         self.assertIsInstance(self.person.email, str)
@@ -40,6 +40,7 @@ class UserCase(unittest.TestCase):
         self.assertIsInstance(self.person.id, str)
         self.assertIsInstance(self.person.created_at, datetime.datetime)
         self.assertIsInstance(self.person.updated_at, datetime.datetime)
+
 
 if __name__ == '__main__':
         unittest.main()
